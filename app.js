14f503b3343daa9c8c0b0bda1c1a60963e5c5c5e
IcySpar <victorasielrodriguez@gmail.com>
2025-08-12 09:50:23 -0700

    build: install packages and test socket srv-client
    testing sockets are working server and client
    side
    
    set up express server and socket.io
    
    testing cors

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { createServer } = require("http");
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+
+const app = express();
+// app.use(cors());
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ['http://localhost:5173']
+  }
+});
+
+app.get('/', (req, res) => {
+  res.json({
+    hello: 'world'
+  })
+});
+
+io.on('connection', (socket) => {
+  console.log('client has connected')
+  socket.emit('connection', 'welcome to the chat!')
+
+
+  socket.on('button press', () => {
+    io.emit('button press', socket.id + ' pressed the button');
+  })
+
+  socket.on('disconnecting', (reason) => {
+    io.emit('user left', socket.id + ' left for some reason');
+  })
+
+})
+
+
+
+httpServer.listen(3000, () => {
+  console.log(`listening on port ${3000}`)
+})
