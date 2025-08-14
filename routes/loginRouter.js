4edb2b3ded78efa283a73ed59d64b805ade3ae24
IcySpar <victorasielrodriguez@gmail.com>
2025-08-14 11:07:32 -0700

    fix: set header err, set token expiration to 10hrs

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,28 +1,25 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const loginRouter = Router();
-const passport = require('passport');
-const { loginUser } = require('../controllers/loginController.js');
-const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const { loginUser } = require("../controllers/loginController.js");
+const jwt = require("jsonwebtoken");
 
-loginRouter.post('/', (req, res, next) => {
-
-  passport.authenticate('local', { session: false },
-    (err, user, info) => {
-      if(err || !user) {
-        return res.status(400).json({
-          message: 'Something is not right',
-        });
-      }
+loginRouter.post("/", (req, res, next) => {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (!err || user) {
       req.login(user, { session: false }, (err) => {
-        if(err) {
-          res.send(err);
-        };
-        const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1m' });
-        return res.json({token});
+        const token = jwt.sign(user, process.env.JWT_SECRET, {
+          expiresIn: "10h",
+        });
+        return res.json({ token });
       });
-    })(req, res);
-
-
-}, loginUser)
+    }
+    if (err || !user) {
+      return res.status(401).json({
+        message: "Something is not right",
+      });
+    }
+  })(req, res);
+});
 
 module.exports = loginRouter;
