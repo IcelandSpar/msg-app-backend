3011b4ac71865ff23795d9eb5fc798bddcd417b6
IcySpar <victorasielrodriguez@gmail.com>
2025-09-12 20:12:37 -0700

    feat: add route to get messages with profile info

--- controllers/directMessageController.js
+++ controllers/directMessageController.js
@@ -1,30 +1,44 @@
 const prisma = require("../db/prismaClient.js");
 
+const getDirectMessages = async (req, res) => {
+  try {
+    const directMessages = await prisma.directMessage.findMany({
+      where: {
+        directMessageGroupId: req.params.directMessageGroupId,
+      },
+      include: {
+        author: true,
+      }
+    });
+    return res.status(200).json({
+      success: true,
+      directMessages: directMessages,
+    });
+  } catch (err) {
+    
+      return res.status(404).json({ success: false ,message: "Something went wrong..." });
+    
+  }
+};
+
 const postMessage = async (req, res) => {
   try {
+    const messagePosted = await prisma.directMessage.create({
+      data: {
+        authorId: req.body.authorId,
+        directMessageGroupId: req.body.directMessageGroupId,
+        messageContent: req.body.message,
+      },
+    });
 
-
-
-       const messagePosted = await prisma.directMessage.create({
-        data: {
-          authorId: req.body.authorId,
-          directMessageGroupId: req.body.directMessageGroupId,
-          messageContent: req.body.message,
- 
-        },
-      })
-
-
-      const updatedDirectMessages = await prisma.directMessage.findMany({
-        where: {
-          directMessageGroupId: directMessageGroupId,
-        },
-        orderBy: {
-          createdAt: 'asc'
-        }
-      })
-
-    
+    const updatedDirectMessages = await prisma.directMessage.findMany({
+      where: {
+        directMessageGroupId: directMessageGroupId,
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
 
     return res.status(200).json({
       success: true,
@@ -39,5 +53,6 @@ const postMessage = async (req, res) => {
 };
 
 module.exports = {
+  getDirectMessages,
   postMessage,
 };
--- routes/directMessageRouter.js
+++ routes/directMessageRouter.js
@@ -1,8 +1,9 @@
 const passport = require('passport');
 const { Router } = require('express');
 const directMessageRouter = Router();
-const { postMessage } = require('../controllers/directMessageController.js');
+const { getDirectMessages, postMessage } = require('../controllers/directMessageController.js');
 
+directMessageRouter.get('/get-direct-messages/:directMessageGroupId', passport.authenticate('jwt', {session: false}), getDirectMessages);
 
 directMessageRouter.post('/post-message', passport.authenticate('jwt', {session: false}), postMessage);
 
