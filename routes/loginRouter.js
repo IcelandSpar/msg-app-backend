d35b09dea7b32da080db4a856f480def7efdf8e5
IcySpar <victorasielrodriguez@gmail.com>
2025-11-06 22:22:56 -0800

    feat: add formValidation to login route

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,11 +1,19 @@
 const { Router } = require("express");
 const loginRouter = Router();
 const passport = require("passport");
+const {  validationResult, matchedData } = require("express-validator");
+const { validateLogin } = require("../validators/loginValidator.js");
 const { loginUser } = require("../controllers/loginController.js");
 const jwt = require("jsonwebtoken");
 
-loginRouter.post("/", (req, res, next) => {
+loginRouter.post("/", validateLogin,(req, res, next) => {
   passport.authenticate("local", { session: false }, (err, user, info) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+    })
+    }
     if (!err || user) {
       req.login(user, { session: false }, (err) => {
         delete user["password"];
---
+++ validators/loginValidator.js
@@ -0,0 +1,14 @@
+const { body } = require('express-validator');
+
+const validateLogin = [
+  body("username").trim()
+  .notEmpty().withMessage('Username must not be empty')
+  .isLength({min: 5, max: 25}).withMessage('Username must be between 5 - 25 characters'),
+  body("password").trim()
+  .notEmpty().withMessage('Password must not be empty')
+  .isLength({min: 5, max: 25}).withMessage('Password must be between 5 - 25 characters')
+];
+
+module.exports = {
+  validateLogin
+};
