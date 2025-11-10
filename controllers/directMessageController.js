9d054e2f80fd2cf3aa43cdd28200f848e09bb3b2
IcySpar <victorasielrodriguez@gmail.com>
2025-11-09 21:57:18 -0800

    feat: add validation to post directMessage route

--- controllers/directMessageController.js
+++ controllers/directMessageController.js
@@ -1,4 +1,6 @@
 const prisma = require("../db/prismaClient.js");
+const { validationResult } = require("express-validator");
+const { validateDirectMsg } = require("../validators/msgValidators.js");
 
 const getDirectMessages = async (req, res) => {
   try {
@@ -8,49 +10,59 @@ const getDirectMessages = async (req, res) => {
       },
       include: {
         author: true,
-      }
+      },
     });
     return res.status(200).json({
       success: true,
       directMessages: directMessages,
     });
   } catch (err) {
-    
-      return res.status(404).json({ success: false ,message: "Something went wrong..." });
-    
+    return res
+      .status(404)
+      .json({ success: false, message: "Something went wrong..." });
   }
 };
 
-const postMessage = async (req, res) => {
-  try {
-    const messagePosted = await prisma.directMessage.create({
-      data: {
-        authorId: req.body.authorId,
-        directMessageGroupId: req.body.directMessageGroupId,
-        messageContent: req.body.message,
-      },
-    });
+const postMessage = [
+  validateDirectMsg,
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+        });
+      } else {
+        const messagePosted = await prisma.directMessage.create({
+          data: {
+            authorId: req.body.authorId,
+            directMessageGroupId: req.body.directMessageGroupId,
+            messageContent: req.body.message,
+          },
+        });
 
-    const updatedDirectMessages = await prisma.directMessage.findMany({
-      where: {
-        directMessageGroupId: req.body.directMessageGroupId,
-      },
-      orderBy: {
-        createdAt: "asc",
-      },
-    });
+        const updatedDirectMessages = await prisma.directMessage.findMany({
+          where: {
+            directMessageGroupId: req.body.directMessageGroupId,
+          },
+          orderBy: {
+            createdAt: "asc",
+          },
+        });
 
-    return res.status(200).json({
-      success: true,
-      message: messagePosted,
-      updatedDirectMessages: updatedDirectMessages,
-    });
-  } catch (err) {
-    if (err) {
-      console.error(err);
+        return res.status(200).json({
+          success: true,
+          message: messagePosted,
+          updatedDirectMessages: updatedDirectMessages,
+        });
+      }
+    } catch (err) {
+      if (err) {
+        console.error(err);
+      }
     }
-  }
-};
+  },
+];
 
 module.exports = {
   getDirectMessages,
